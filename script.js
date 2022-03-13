3435bbe50d80dd6bc8510bc582ae599f767300d9
jalal <jalal.rafiyev@gmail.com>
2022-03-13 20:12:51 +0400

    done with the exercise

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let result = "";
+
+
 function computerPlay() {
   let random = Math.floor(Math.random() * 3);
 
@@ -12,31 +15,76 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "It's a tie!";
+    result = "It's a tie!";
+    console.log("It's a tie!");
+    div.textContent = result;
+    return result;
   } else if (playerSelection == "rock") {
     if (computerSelection == "scissors") {
-      return "You win!";
+      result = "You win!";
+      console.log("You win!");
+      div.textContent = result;
+      return result;
     } else {
-      return "You lose!";
+      result = "You lose!";
+      console.log("You lose!");
+      div.textContent = result;
+      return result;
     }
   } else if (playerSelection == "paper") {
     if (computerSelection == "rock") {
-      return "You win!";
+      result = "You win!";
+      console.log("You win!");
+      div.textContent = result;
+      return result;
     } else {
-      return "You lose!";
+      result = "You lose!";
+      console.log("You lose!");
+      div.textContent = result;
+      return result;
     }
   } else if (playerSelection == "scissors") {
     if (computerSelection == "paper") {
-      return "You win!";
+      result = "You win!";
+      console.log("You win!");
+      div.textContent = result;
+      return result;
     } else {
-      return "You lose!";
+      result = "You lose!";
+      console.log("You lose!");
+      div.textContent = result;
+      return result;
     }
   }
+  return result;
 }
 
 
-for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, paper, or scissors?");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-}
+const btnRock = document.createElement("button");
+btnRock.addEventListener('click', () => playRound('rock', computerPlay()));
+btnRock.innerHTML = "Rock";
+btnRock.style.padding = "75px 75px";
+btnRock.style.margin = "10px";
+document.body.appendChild(btnRock);
+
+const btnPaper = document.createElement("button");
+btnPaper.addEventListener('click', () => playRound('paper', computerPlay()));
+btnPaper.innerHTML = "Paper";
+btnPaper.style.padding = "75px 75px";
+btnPaper.style.margin = "10px";
+document.body.appendChild(btnPaper);
+
+const btnScissors = document.createElement("button");
+btnScissors.addEventListener('click', () => playRound('scissors', computerPlay()));
+btnScissors.innerHTML = "Scissors";
+btnScissors.style.padding = "75px 75px";
+btnScissors.style.margin = "10px";
+document.body.appendChild(btnScissors);
+
+const div = document.createElement("div");
+div.style.textAlign = "center";
+div.style.margin = "10px";
+div.style.border = "1px solid black";
+div.style.padding = "100px";
+div.style.width = "300px";
+document.body.appendChild(div);
